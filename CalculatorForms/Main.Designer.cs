499d57b91cc140a417f78f5052eec67ae997e028
Diogo <antoninidiogo@gmail.com>
2022-08-31 11:07:37 -0300

    zz

--- CalculatorForms/Main.Designer.cs
+++ CalculatorForms/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CalculatorForms
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(487, 664);
+            this.Name = "Main";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- CalculatorForms/Main.cs
+++ CalculatorForms/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CalculatorForms
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
